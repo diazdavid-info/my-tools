1d356df3c393b3fef058f7ea21d4ea4334f7cf61
David Diaz <diazdavid.info@gmail.com>
2023-08-21 19:57:15 +0200

    add fetch

--- apps/tasks/index.ts
+++ apps/tasks/index.ts
@@ -1,11 +1,30 @@
 #!/usr/bin/env node
 
+import prompts from 'prompts'
+import { simpleGit } from 'simple-git'
+
 const handleSigTerm = () => process.exit(0)
 
 process.on('SIGINT', handleSigTerm)
 process.on('SIGTERM', handleSigTerm)
 
-async function run(): Promise<void> {}
+async function createBranch(): Promise<void> {
+  const response = await simpleGit().fetch()
+
+  console.log(response)
+}
+
+async function run(): Promise<void> {
+  const { task } = await prompts({
+    type: 'select',
+    name: 'task',
+    message: 'What do you want to do?',
+    choices: [{ title: 'Create branch', description: '', value: 'Create branch' }],
+    initial: 0
+  })
+
+  if (task === 'Create branch') await createBranch()
+}
 
 run()
   .then(() => {
