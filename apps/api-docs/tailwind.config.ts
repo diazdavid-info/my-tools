a197e35c512ee786388b9459587455515719a81b
David Diaz <diazdavid.info@gmail.com>
2023-12-23 20:31:01 +0100

    install contentlayer and shadcn

--- apps/api-docs/tailwind.config.ts
+++ apps/api-docs/tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss'
+import { fontFamily } from 'tailwindcss/defaultTheme'
 
 const config: Config = {
   content: [
@@ -12,6 +13,9 @@ const config: Config = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      fontFamily: {
+        sans: ["var(--font-sans)", ...fontFamily.sans],
       },
     },
   },
