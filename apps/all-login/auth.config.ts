138e542af4a8f8de18f5b2233a3881770dc19051
David Diaz <diazdavid.info@gmail.com>
2024-01-25 11:37:28 +0100

    add google auth

--- apps/all-login/auth.config.ts
+++ apps/all-login/auth.config.ts
@@ -2,6 +2,7 @@
 import GitHub from '@auth/core/providers/github'
 import Atlassian from '@auth/core/providers/atlassian'
 import { defineConfig } from 'auth-astro'
+import Google from "@auth/core/providers/google";
 
 export default defineConfig({
   providers: [
@@ -23,5 +24,9 @@ export default defineConfig({
         }
       }
     }),
+    Google({
+      clientId: import.meta.env.GOOGLE_CLIENT_ID,
+      clientSecret: import.meta.env.GOOGLE_CLIENT_SECRET
+    })
   ],
 });
