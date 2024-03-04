1c70457cc75fd598900f25d72f8a185b46c3205a
David Diaz <diazdavid.info@gmail.com>
2024-03-04 12:55:26 +0100

    add function me and storage token

--- apps/oauth-expressjs/README.md
+++ apps/oauth-expressjs/README.md
@@ -4,3 +4,16 @@
 * https://developers.google.com/identity/protocols/oauth2/web-server?hl=es-419#httprest
 * https://developers.google.com/identity/protocols/oauth2/web-server?hl=es-419#node.js
 * [Motivos de expiración del refresh token](https://developers.google.com/identity/protocols/oauth2?hl=es-419#expiration)
+
+
+## Base de datos
+```
+CREATE TABLE `user` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(100) NOT NULL DEFAULT '',
+  `token` text NOT NULL,
+  `refreshToken` text NOT NULL,
+  `expiredDate` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+```
--- apps/oauth-expressjs/index.js
+++ apps/oauth-expressjs/index.js
@@ -1,16 +1,30 @@
 import express from 'express'
 import { google } from 'googleapis'
+import bcrypt from 'bcryptjs'
+import knex from 'knex'
 
 const app = express()
 const port = 3000
 const REDIRECT_URL = 'http://localhost:3000/login/callback'
+
+const connection = knex({
+  client: 'mysql2',
+  connection: {
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT ?? '3306'),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+  }
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.get('/login/callback', async (req, res) => {
-  const { state, code, hd } = req.query
+  const { state, code } = req.query
+  if (!state || !bcrypt.compare(state, process.env.AUTH_SECRET)) throw new Error('error')
   const response = await fetch('https://oauth2.googleapis.com/token', {
     method: 'POST',
     body: JSON.stringify({
@@ -27,19 +41,21 @@ app.get('/login/callback', async (req, res) => {
 })
 
 app.get('/login', (req, res) => {
+  const state = bcrypt.hash(process.env.AUTH_SECRET, 10)
   const queryParams = new URLSearchParams({
     scope: 'https://www.googleapis.com/auth/userinfo.profile',
     include_granted_scopes: true,
     response_type: 'code',
     access_type: 'offline',
-    state: 'state_parameter_passthrough_value',
+    state,
     redirect_uri: REDIRECT_URL,
     client_id: process.env.GOOGLE_CLIENT_ID
   })
   res.redirect(`https://accounts.google.com/o/oauth2/v2/auth?${queryParams.toString()}`)
 })
 
-app.get('/sdk/login', (req, res) => {
+app.get('/sdk/login', async (req, res) => {
+  const state = bcrypt.hash(process.env.AUTH_SECRET, 10)
   const oauth2Client = new google.auth.OAuth2(
     process.env.GOOGLE_CLIENT_ID,
     process.env.GOOGLE_CLIENT_SECRET,
@@ -48,7 +64,8 @@ app.get('/sdk/login', (req, res) => {
   const authorizationUrl = oauth2Client.generateAuthUrl({
     access_type: 'offline',
     scope: ['https://www.googleapis.com/auth/userinfo.profile'],
-    include_granted_scopes: true
+    include_granted_scopes: true,
+    state
   })
   res.redirect(authorizationUrl)
 })
@@ -59,9 +76,75 @@ app.get('/sdk/login/callback', async (req, res) => {
     process.env.GOOGLE_CLIENT_SECRET,
     'http://localhost:3000/sdk/login/callback'
   )
-  const { tokens } = await oauth2Client.getToken(req.query.code)
+  const getTokenResponse = await oauth2Client.getToken(req.query.code)
+  const { tokens } = getTokenResponse
+  const { access_token, refresh_token, expiry_date } = tokens
 
-  res.json(tokens)
+  oauth2Client.setCredentials(tokens)
+  const oauth2 = google.oauth2({
+    auth: oauth2Client,
+    version: 'v2'
+  })
+  const { data } = await oauth2.userinfo.get()
+  const { email } = data
+
+  const user = await connection('user').where('email', '=', email).first()
+  if (user) {
+    await connection('user')
+      .update({
+        token: access_token,
+        refreshToken: refresh_token,
+        expiredDate: new Date(expiry_date)
+      })
+      .where('id', '=', user.id)
+    return res.json({ tokens, email })
+  }
+
+  await connection('user').insert({
+    email,
+    token: access_token,
+    refreshToken: refresh_token,
+    expiredDate: new Date(expiry_date)
+  })
+
+  res.json({ tokens, email })
+})
+
+app.get('/me/:email', async (req, res) => {
+  const { email } = req.params
+  const user = await connection('user').where('email', '=', email).first()
+  if (!user) {
+    return res.send('MAL', 401)
+  }
+
+  const oauth2Client = new google.auth.OAuth2(
+    process.env.GOOGLE_CLIENT_ID,
+    process.env.GOOGLE_CLIENT_SECRET,
+    'http://localhost:3000/sdk/login/callback'
+  )
+  oauth2Client.on('tokens', (tokens) => {
+    console.log(tokens)
+    if (tokens.refresh_token) {
+      // store the refresh_token in your secure persistent database
+      console.log('tokens.refresh_token')
+      console.log(tokens.refresh_token)
+    }
+    console.log('tokens.access_token')
+    console.log(tokens.access_token)
+  })
+
+  oauth2Client.setCredentials({
+    access_token: user.token,
+    refresh_token: user.refreshToken
+  })
+
+  const oauth2 = google.oauth2({
+    auth: oauth2Client,
+    version: 'v2'
+  })
+
+  const { data } = await oauth2.userinfo.get()
+  res.json(data)
 })
 
 app.listen(port, () => {
