39c1345acf6627a9f61733aa879f249b7b0b4741
David Diaz <diazdavid.info@gmail.com>
2024-04-26 19:24:05 +0200

    typing library

--- packages/typing-test/index.test.ts
+++ packages/typing-test/index.test.ts
@@ -1,5 +1,206 @@
-import { expect, it } from 'vitest'
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
+import { typing } from './index'
+import { fireEvent, waitFor } from '@testing-library/dom'
 
-it('Should nothing', async () => {
-  expect(true).toBeTruthy()
+describe('typing', () => {
+  beforeEach(() => {
+    vi.useFakeTimers()
+
+    document.body.innerHTML = `
+        <p data-text="foo" id="element">
+            <span data-right-char="b">b</span>
+            <span data-right-char="a">a</span>
+            <span data-right-char="r">r</span>
+       </p>`
+  })
+  afterEach(() => {
+    vi.useRealTimers()
+  })
+
+  it('Should has focus when init typing', async () => {
+    const finishCallback = vi.fn()
+
+    const result = typing({
+      readElement: selector<HTMLParagraphElement>('#element'),
+      rightClass: 'correct',
+      wrongClass: 'incorrect',
+      finishCallback
+    })
+
+    expect(document.activeElement).toBe(result)
+  })
+  it('Should call to callback when user typing all characters', async () => {
+    const finishCallback = vi.fn()
+
+    const result = typing({
+      readElement: selector<HTMLParagraphElement>('#element'),
+      rightClass: 'correct',
+      wrongClass: 'incorrect',
+      finishCallback
+    })
+
+    fireEvent.change(result, { target: { value: 'ptu' } })
+
+    await waitFor(() => expect(finishCallback).toHaveBeenCalledTimes(1))
+  })
+  it('Should add successClass in correct elements', async () => {
+    const finishCallback = vi.fn()
+
+    const result = typing({
+      readElement: selector<HTMLParagraphElement>('#element'),
+      rightClass: 'correct',
+      wrongClass: 'incorrect',
+      finishCallback
+    })
+
+    fireEvent.change(result, { target: { value: 'eaw' } })
+
+    await waitFor(() => {
+      const correctElements = selectorAll<HTMLSpanElement[]>('.correct')
+      const correctCharacters = correctElements.map((element: HTMLSpanElement) => element.innerText)
+
+      expect(correctElements.length).toBe(1)
+      expect(Array.from(correctCharacters)).toEqual(['a'])
+    })
+  })
+  it('Should add successClass when typing remove and typing correct character', async () => {
+    const finishCallback = vi.fn()
+
+    const result = typing({
+      readElement: selector<HTMLParagraphElement>('#element'),
+      rightClass: 'correct',
+      wrongClass: 'incorrect',
+      finishCallback
+    })
+
+    fireEvent.change(result, { target: { value: 'a' } })
+
+    await waitFor(() => {
+      const inCorrectElements = selectorAll<HTMLSpanElement[]>('.incorrect')
+      const inCorrectCharacters = inCorrectElements.map((element: HTMLSpanElement) => element.innerText)
+
+      expect(inCorrectElements.length).toBe(1)
+      expect(Array.from(inCorrectCharacters)).toEqual(['b'])
+    })
+
+    fireEvent.change(result, { target: { value: 'b' } })
+
+    await waitFor(() => {
+      const inCorrectElements = selectorAll<HTMLSpanElement[]>('.incorrect')
+
+      expect(inCorrectElements.length).toBe(0)
+
+      const correctElements = selectorAll<HTMLSpanElement[]>('.correct')
+      const correctCharacters = correctElements.map((element: HTMLSpanElement) => element.innerText)
+
+      expect(correctElements.length).toBe(1)
+      expect(Array.from(correctCharacters)).toEqual(['b'])
+    })
+  })
+  it('Should add wrongClass in incorrect elements', async () => {
+    const finishCallback = vi.fn()
+
+    const result = typing({
+      readElement: selector<HTMLParagraphElement>('#element'),
+      rightClass: 'correct',
+      wrongClass: 'incorrect',
+      finishCallback
+    })
+
+    fireEvent.change(result, { target: { value: 'eaw' } })
+
+    await waitFor(() => {
+      const correctElements = selectorAll<HTMLSpanElement[]>('.incorrect')
+      const correctCharacters = correctElements.map((element: HTMLSpanElement) => element.innerText)
+
+      expect(correctElements.length).toBe(2)
+      expect(Array.from(correctCharacters)).toEqual(['b', 'r'])
+    })
+  })
+  it('Should call finish callback with count wrong characters', async () => {
+    const finishCallback = vi.fn()
+
+    const result = typing({
+      readElement: selector<HTMLParagraphElement>('#element'),
+      rightClass: 'correct',
+      wrongClass: 'incorrect',
+      finishCallback
+    })
+
+    fireEvent.change(result, { target: { value: 'eaw' } })
+
+    await waitFor(() => expect(finishCallback).toBeCalled())
+    expect(finishCallback).toHaveBeenCalledWith(expect.objectContaining({ wrongCounter: 2 }))
+  })
+  it('Should call finish callback with time in seconds', async () => {
+    const finishCallback = vi.fn()
+    vi.setSystemTime(new Date(2000, 0, 1, 0, 0, 0))
+
+    const result = typing({
+      readElement: selector<HTMLParagraphElement>('#element'),
+      rightClass: 'correct',
+      wrongClass: 'incorrect',
+      finishCallback
+    })
+
+    fireEvent.change(result, { target: { value: 'e' } })
+
+    await waitFor(() => {
+      const correctElements = selectorAll<HTMLSpanElement[]>('.incorrect')
+      expect(correctElements.length).toBe(1)
+    })
+
+    vi.setSystemTime(new Date(2000, 0, 1, 0, 0, 10))
+    fireEvent.change(result, { target: { value: 'bar' } })
+
+    await waitFor(() => expect(finishCallback).toBeCalled())
+    expect(finishCallback).toHaveBeenCalledWith(expect.objectContaining({ secondsStart: 10 }))
+  })
+  it('Should call finish callback with charactersMinute in seconds', async () => {
+    const finishCallback = vi.fn()
+    vi.setSystemTime(new Date(2000, 0, 1, 0, 0, 0))
+
+    const result = typing({
+      readElement: selector<HTMLParagraphElement>('#element'),
+      rightClass: 'correct',
+      wrongClass: 'incorrect',
+      finishCallback
+    })
+
+    fireEvent.change(result, { target: { value: 'b' } })
+
+    await waitFor(() => {
+      const correctElements = selectorAll<HTMLSpanElement[]>('.correct')
+
+      expect(correctElements.length).toBe(1)
+    })
+
+    vi.setSystemTime(new Date(2000, 0, 1, 0, 0, 3))
+    fireEvent.change(result, { target: { value: 'bar' } })
+
+    await waitFor(() => expect(finishCallback).toBeCalled())
+    expect(finishCallback).toHaveBeenCalledWith(expect.objectContaining({ charactersMinute: 60 }))
+  })
+  it('Should call finish callback with wroteCounter', async () => {
+    const finishCallback = vi.fn()
+
+    const result = typing({
+      readElement: selector<HTMLParagraphElement>('#element'),
+      rightClass: 'correct',
+      wrongClass: 'incorrect',
+      finishCallback
+    })
+
+    fireEvent.change(result, { target: { value: 'b z' } })
+
+    await waitFor(() => expect(finishCallback).toBeCalled())
+    expect(finishCallback).toHaveBeenCalledWith(expect.objectContaining({ wroteCounter: 3 }))
+  })
+
+  function selectorAll<T>(query: string): T {
+    return document.querySelectorAll(query) as unknown as T
+  }
+  function selector<T>(query: string): T {
+    return document.querySelector(query) as unknown as T
+  }
 })
--- packages/typing-test/index.ts
+++ packages/typing-test/index.ts
@@ -1,1 +1,123 @@
-export function typing() {}
+export type OptionsTyping = {
+  readElement: HTMLElement
+  rightClass: string
+  wrongClass: string
+  finishCallback: ({
+    wrongCounter,
+    secondsStart,
+    charactersMinute,
+    wroteCounter
+  }: {
+    wrongCounter: number
+    secondsStart: number
+    charactersMinute: number
+    wroteCounter: number
+  }) => void
+}
+
+export function typing(options: OptionsTyping) {
+  const input = createInput()
+  const spanElements = options.readElement.querySelectorAll('span')
+
+  run(input, spanElements, options)
+
+  return input
+}
+
+function run(input: HTMLInputElement, spanElements: NodeListOf<HTMLSpanElement>, options: OptionsTyping) {
+  const textWrote = input.value
+  const textWroteList = textWrote.split('')
+
+  input.focus()
+
+  spanElements.forEach((spanElement, index) => {
+    spanElement.classList.remove(options.rightClass, options.wrongClass)
+    const isFirstInputChar = textWroteList.length === 1 && !spanElements[0].dataset.startCurrentTime
+    const isRightChar = spanElement.dataset.rightChar === textWroteList[index]
+    const isValidChar = /\w|\s|,|\./.test(textWroteList[index])
+    const isWrongChar = isValidChar && textWroteList[index] !== undefined && !isRightChar
+
+    if (isFirstInputChar) spanElements[0].dataset.startCurrentTime = (new Date().valueOf() / 1000).toString()
+
+    if (isRightChar) spanElement.classList.add(options.rightClass)
+    if (isWrongChar) {
+      spanElement.classList.add(options.wrongClass)
+      spanElement.dataset.wrongChar = textWroteList[index]
+    }
+  })
+
+  const rightCharacters = options.readElement.querySelectorAll(`.${options.rightClass}`).length
+  const wrongCharacters = options.readElement.querySelectorAll('[data-wrong-char]').length
+  const startCurrentTime = parseInt(spanElements[0].dataset.startCurrentTime ?? '0')
+
+  if (textWroteList.length === spanElements.length) {
+    options.finishCallback &&
+      options.finishCallback({
+        wrongCounter: wrongCharacters,
+        secondsStart: startCurrentTime ? Math.round(new Date().valueOf() / 1000 - startCurrentTime) : 0,
+        charactersMinute: Math.round((60 * rightCharacters) / (new Date().valueOf() / 1000 - startCurrentTime)),
+        wroteCounter: textWroteList.length
+      })
+    // input.blur()
+    return
+  }
+
+  //Math.round((60 * rightCharacters) / (new Date().valueOf() / 1000 - startCurrentTime)),
+
+  window.requestAnimationFrame(() => run(input, spanElements, options))
+
+  // const textWrote = input.value
+  // const textWroteList = textWrote.split('')
+  //
+  // input.focus()
+  //
+  // spanElements.forEach((spanElement, index) => {
+  //   spanElement.classList.remove('text-violet-600', 'text-red-500')
+  //   const isFirstInputChar = textWroteList.length === 1 && !spanElements[0].dataset.startCurrentTime
+  //   const isRightChar = spanElement.dataset.rightChar === textWroteList[index]
+  //   const isValidChar = /\w|\s|,|\./.test(textWroteList[index])
+  //   const isWrongChar = isValidChar && textWroteList[index] !== undefined && !isRightChar
+  //
+  //   if (isFirstInputChar) spanElements[0].dataset.startCurrentTime = (new Date().valueOf() / 1000).toString()
+  //   if (isRightChar) spanElement.classList.add('text-violet-600')
+  //   if (isWrongChar) {
+  //     spanElement.classList.add('text-red-500')
+  //     spanElement.dataset.wrongChar = textWroteList[index]
+  //   }
+  // })
+  //
+  // const wrongCharacters = options.readElement.querySelectorAll('[data-wrong-char]').length
+  // const rightCharacters = options.readElement.querySelectorAll('.text-violet-600').length
+  // const startCurrentTime = parseInt(spanElements[0].dataset.startCurrentTime ?? '0')
+  //
+  // options.statisticsCallback &&
+  //   options.statisticsCallback({
+  //     wrongCounter: wrongCharacters,
+  //     wroteCounter: textWroteList.length,
+  //     charactersMinute: Math.round((60 * rightCharacters) / (new Date().valueOf() / 1000 - startCurrentTime)),
+  //     wordsMinute: 0, //Math.round((60 * countWords) / (new Date().valueOf() / 1000 - startCurrentTime))
+  //     secondsStart: startCurrentTime ? Math.round(new Date().valueOf() / 1000 - startCurrentTime) : 0
+  //   })
+  //
+  // if (textWroteList.length === spanElements.length) {
+  //   options.finishCallback && options.finishCallback()
+  //   input.blur()
+  //   return
+  // }
+  //
+  // window.requestAnimationFrame(() => run(input, spanElements, options))
+}
+
+function createInput() {
+  const input = document.createElement('input')
+  input.setAttribute('type', 'text')
+  // input.setAttribute('aria-label', 'Text')
+  // input.setAttribute('autocomplete', 'off')
+  // input.setAttribute('autocapitalize', 'off')
+  // input.setAttribute('inputmode', 'text')
+  // input.setAttribute('autocorrect', 'off')
+  // input.setAttribute('spellcheck', 'false')
+  // if(!debug) input.classList.add('opacity-0')
+  document.querySelector('body')?.appendChild(input)
+  return input
+}
