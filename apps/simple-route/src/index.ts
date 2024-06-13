037a84fbfe9dd647e79ff57701a7a2bb0f4707f0
David Diaz <diazdavid.info@gmail.com>
2024-06-13 09:30:12 +0200

    change host

--- apps/simple-route/src/index.ts
+++ apps/simple-route/src/index.ts
@@ -4,6 +4,6 @@ const server = net.createServer((socket) => {
   socket.end()
 })
 
-server.listen(4221, 'localhost', () => {
-  console.log('Server is running on port 4221')
+server.listen(4221, '127.0.0.1', () => {
+  console.log('Server is running on http://127.0.0.1:4221')
 })
