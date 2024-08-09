f54a44da37eaed7ffc6ebf03c544277bdbdea0cd
David Diaz <diazdavid.info@gmail.com>
2024-08-09 14:13:53 +0200

    add tailwind

--- apps/monaco-editor/astro.config.mjs
+++ apps/monaco-editor/astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import cloudflare from "@astrojs/cloudflare";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  integrations: [tailwind()]
 });
