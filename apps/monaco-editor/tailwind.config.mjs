37460e5d23558f84cd3b4ecff3744e7895ef655d
David Diaz <diazdavid.info@gmail.com>
2024-08-09 18:14:34 +0200

    new design

--- apps/monaco-editor/tailwind.config.mjs
+++ apps/monaco-editor/tailwind.config.mjs
@@ -3,6 +3,11 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		colors: {
+			'primary': '#fff',
+			'secondary': '#f4f4f5',
+			'accent': '#6e6e77',
+		}
 	},
 	plugins: [],
 }
