921206c67aab597d576fce1fbe3abef8f05463ca
David Diaz <diazdavid.info@gmail.com>
2024-08-10 10:06:29 +0200

    dark mode

--- apps/monaco-editor/tailwind.config.mjs
+++ apps/monaco-editor/tailwind.config.mjs
@@ -2,12 +2,16 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
-		colors: {
-			'primary': '#fff',
-			'secondary': '#f4f4f5',
-			'accent': '#6e6e77',
-		}
+		extend: {
+			colors: {
+				'primary': '#fff',
+				'secondary': '#f4f4f5',
+				'accent': '#6e6e77',
+				'primary-dark': '#1e1e1e',
+				'secondary-dark': '#000',
+			}
+		},
 	},
+	darkMode: 'selector',
 	plugins: [],
 }
