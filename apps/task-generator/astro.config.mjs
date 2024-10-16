cc2c56f8e742d30e7c060ee7e2deeb64c8e89ee4
David Diaz <diazdavid.info@gmail.com>
2024-10-16 16:16:21 +0200

    change adapter

--- apps/task-generator/astro.config.mjs
+++ apps/task-generator/astro.config.mjs
@@ -4,11 +4,13 @@ import tailwind from "@astrojs/tailwind";
 import auth from "auth-astro";
 import vercel from "@astrojs/vercel/serverless";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind({
     applyBaseStyles: false
   }), auth()],
   output: "server",
-  adapter: vercel()
+  adapter: cloudflare()
 });
