2872502a9329855a6699c7df3b977fdb9100ed95
David Diaz <diazdavid.info@gmail.com>
2024-11-23 10:13:09 +0100

    add cloudflare

--- apps/blog/astro.config.mjs
+++ apps/blog/astro.config.mjs
@@ -5,30 +5,37 @@ import tailwind from '@astrojs/tailwind'
 import { remarkReadingTime } from './src/utils/readTime.ts'
 import { siteConfig } from './src/data/site.config'
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
-	site: siteConfig.site,
-	markdown: {
-		remarkPlugins: [remarkReadingTime],
-		drafts: true,
-		shikiConfig: {
-			theme: 'material-theme-palenight',
-			wrap: true
-		}
+  site: siteConfig.site,
+
+  markdown: {
+      remarkPlugins: [remarkReadingTime],
+      drafts: true,
+      shikiConfig: {
+          theme: 'material-theme-palenight',
+          wrap: true
+      }
 	},
-	integrations: [
-		mdx({
-			syntaxHighlight: 'shiki',
-			shikiConfig: {
-				experimentalThemes: {
-					light: 'vitesse-light',
-					dark: 'material-theme-palenight',
-				  },
-				wrap: true
-			},
-			drafts: true
-		}),
-		sitemap(),
-		tailwind()
-	]
+
+  integrations: [
+      mdx({
+          syntaxHighlight: 'shiki',
+          shikiConfig: {
+              experimentalThemes: {
+                  light: 'vitesse-light',
+                  dark: 'material-theme-palenight',
+                },
+              wrap: true
+          },
+          drafts: true
+      }),
+      sitemap(),
+      tailwind()
+	],
+
+  output: 'server',
+  adapter: cloudflare()
 })
